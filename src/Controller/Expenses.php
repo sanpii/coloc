691d03b14a23cc419b779bee24ffe21782af041e
Sanpi <sanpi@homecomputing.fr>
2013-09-10 22:45:58 +0200

    Use new Pager::getMaxPerPage method

--- src/Controller/Expenses.php
+++ src/Controller/Expenses.php
@@ -46,7 +46,7 @@ class Expenses implements ControllerProviderInterface
 
         return $app['twig']->render(
             'index.html.twig',
-            compact('pager', 'limit')
+            compact('pager')
         );
     }
 
