b930c62b990cc614d385ed72cef09588522f4b7e
Sanpi <sanpi@homecomputing.fr>
2017-11-21 16:44:04 +0100

    Fix default env & debug

--- public/index.php
+++ public/index.php
@@ -9,14 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
-$env = getenv('APP_ENV') ?: 'prod';
+$env = getenv('APP_ENV') ?? 'prod';
 $dotenv = __DIR__ . '/../.env';
 if (is_file($dotenv) && $env === 'dev' && class_exists(Dotenv::class)) {
     (new Dotenv)
         ->load($dotenv);
 }
 
-$debug = getenv('APP_DEBUG') ?: false;
+$debug = getenv('APP_DEBUG') ?? false;
 if ($debug && class_exists(Debug::class)) {
     umask(0000);
 
@@ -25,7 +25,7 @@ if ($debug && class_exists(Debug::class)) {
 
 // Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
 
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', $_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev')));
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
